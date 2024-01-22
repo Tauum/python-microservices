81271a7adea56b9edd0a78d2110caae9fa430ea2
Thomas Storey <thomas.storey@netcompany.com>
2024-01-22 10:48:19 +0000

    initial - failing because docker sucks fat cock

---
+++ main/producer.py
@@ -0,0 +1,12 @@
+import pika, json
+
+params = pika.URLParameters('amqps://vzqbgtbb:kJyeCvI7IDTzcbowv2_P_tZ7X5-a6g5N@rattlesnake.rmq.cloudamqp.com/vzqbgtbb')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
